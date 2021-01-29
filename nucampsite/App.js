39ec0b117d3c3199396430a4a0d582c974f3754b
Hector Romero <hectorr_39@hotmail.com>
2021-01-29 00:25:25 -0600

    Component Part 1
    Added first two components to the project

---
+++ nucampsite/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import Main from './components/MainComponent';
+
+
+export default function App() {
+  return (
+    <Main />
+  );
+}
+
