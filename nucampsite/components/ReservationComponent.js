3b91e22f5cc74ba16393ce55c719645cb682efd7
Hector Romero <hectorr_39@hotmail.com>
2021-02-18 13:05:51 -0600

    Local Notifications

--- nucampsite/components/ReservationComponent.js
+++ nucampsite/components/ReservationComponent.js
@@ -5,6 +5,7 @@ import {
 } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
     constructor(props) {
@@ -22,22 +23,6 @@ class Reservation extends Component {
         title: "Reserve Campsite"
     }
 
-    alertButton = () => {
-        Alert.alert(
-            "Begin Search?",
-            "Description",
-            [
-                {
-                    text: "Cancel",
-                    onPress: () => console.log("Cancel Pressed"),
-                    style: "cancel"
-                },
-                { text: "OK", onPress: () => console.log("OK Pressed") }
-            ],
-            { cancelable: false }
-        );
-    }
-
     resetForm() {
         this.setState({
             campers: 1,
@@ -46,6 +31,32 @@ class Reservation extends Component {
             showCalendar: false,
             showModal: false
         });
+    }
+
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
     }
 
     render() {
@@ -113,10 +124,19 @@ class Reservation extends Component {
                                     [
                                         {
                                             text: "Cancel",
-                                            onPress: () => console.log("Cancel Pressed"),
+                                            onPress: () => {
+                                                console.log("Cancel Pressed");
+                                                this.resetForm();
+                                            },
                                             style: "cancel"
                                         },
-                                        { text: "OK", onPress: () => console.log("OK Pressed") }
+                                        {
+                                            text: "OK",
+                                            onPress: () => {
+                                                this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                                                this.resetForm();
+                                            }
+                                        }
                                     ],
                                     { cancelable: false }
                                 );
